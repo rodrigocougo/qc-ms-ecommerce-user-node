7655ce71d6929b9665c79385d6a565d745a4fa33
Rodrigo Cougo <rodrigocougo@gmail.com>
2021-03-10 12:38:22 -0300

    [add] add cors npm

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 //mongo
 require('./config/mongo');
@@ -13,13 +14,14 @@ require('./models/roleModel');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/userRouter');
 var loginRouter = require('./routes/loginRouter');
-
+/* var cors = require('cors'); */
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors()); // 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,6 +31,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', loginRouter);
+
+// use it before all route definitions
+/* app.use(cors({origin: 'http://localhost:3500'})); */
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -3,13 +3,27 @@ const LoginController = require('../controllers/loginController');
 
 const router = express.Router();
 
-router.use(function(req, res, next) {
-  res.header(
+//router.use(function(req, res, next) {
+  /* res.header(
     "Access-Control-Allow-Headers",
     "x-access-token, Origin, Content-Type, Accept"
-  );
-  next();
-});
+  ); */
+  
+  // Website you wish to allow to connect
+  /* res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true); */
+
+//  next();
+//});
 
 router.post('/signin', LoginController.getAllLoginController);                // - OK
 
